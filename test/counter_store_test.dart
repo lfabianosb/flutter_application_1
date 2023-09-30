e580347d409d446dbfad383ab3152d1d28f35974
Fabiano Bandeira <lfabianosb@gmail.com>
2023-09-30 15:30:15 -0300

    store tests

---
+++ test/counter_store_test.dart
@@ -0,0 +1,68 @@
+import 'package:flutter_application_1/counter_state.dart';
+import 'package:flutter_application_1/counter_store.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late CounterStore sut;
+  
+  setUp(() {
+    sut = CounterStore();
+  });
+
+  group('CounterStore', () {
+    test('should start with SuccessCounterState(0)', () async {
+      // Arrange
+      // Act
+      // Assert
+      expect(sut.state, const SuccessCounterState(value: 0));
+    });
+
+    test('should change to LoadingCounterState and SuccessCounterState(1) after increment', () async {
+      // Arrange
+      loadingStateVerifying() {
+        expect(sut.state, LoadingCounterState());
+      }
+      successStateVerifying() {
+        expect(sut.state, const SuccessCounterState(value: 1));
+      }
+      sut.addListener(loadingStateVerifying);
+      // Act
+      // Assert
+      var future = sut.increment();
+      sut.removeListener(loadingStateVerifying);
+      sut.addListener(successStateVerifying);
+      await future;
+    });
+
+    test('should change to LoadingCounterState and SuccessCounterState(0) after decrement', () async {
+      // Arrange
+      await sut.increment();
+      loadingStateVerifying() {
+        expect(sut.state, LoadingCounterState());
+      }
+      successStateVerifying() {
+        expect(sut.state, const SuccessCounterState(value: 0));
+      }
+      sut.addListener(loadingStateVerifying);
+      // Act
+      // Assert
+      expect(sut.state, const SuccessCounterState(value: 1));
+      var future = sut.decrement();
+      sut.removeListener(loadingStateVerifying);
+      sut.addListener(successStateVerifying);
+      await future;
+    });
+
+    test('should change to ErrorCounterState after decrement', () async {
+      // Arrange
+      errorStateVerifying() {
+        expect(sut.state, const ErrorCounterState(description: 'Numero negativo não permitido'));
+      }
+      // Act
+      // Assert
+      expect(sut.state, const SuccessCounterState(value: 0));
+      sut.addListener(errorStateVerifying);
+      await sut.decrement();
+    });
+  });
+}
